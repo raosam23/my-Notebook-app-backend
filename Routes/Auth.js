9d6ff6c09392e6a5b494fe2aac9e45540d1a6b74
Samarth Rao <raosamarth1234@gmail.com>
2023-05-10 12:31:58 +0530

    Add Success as true or false to the response.json for the login route

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -70,22 +70,25 @@ router.post(
     body("password", "Password cannot be empty").exists(),
   ],
   async (req, res) => {
+    let success = false;
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({success, errors: errors.array() });
     }
     const { email, password } = req.body;
     try {
       let user = await User.findOne({ email });
       if (!user) {
         return res.status(400).json({
+          success,
           error: "Sorry, please try to login with correct credentials",
         });
       }
       const passwordCompare = await bcrypt.compare(password, user.password);
       if (!passwordCompare) {
         return res.status(400).json({
+          success,
           error: "Sorry, please try to login with correct credentials",
         });
       }
@@ -95,7 +98,8 @@ router.post(
         },
       };
       const authToken = jwt.sign(data, JWT_SECRET);
-      res.json({ authToken });
+      success = true;
+      res.json({ success, authToken });
     } catch (error) {
       // Catch errors
       console.error(error.message);
